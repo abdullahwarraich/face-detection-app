7bbf16c495e63f3ff1917a39a717e30a5c77464a
abdullahwarraich678 <abdullahwarraich678@gmail.com>
2023-12-20 22:54:59 +0000

    implemented a method to remove a selected image using a button

--- frontend/src/pages/FaceRecognitionPage.vue
+++ frontend/src/pages/FaceRecognitionPage.vue
@@ -2,14 +2,17 @@
     <div class="image-container">
         <div class="upload-image-container">
             <h2>Upload an image to detect faces in the image</h2>
-            <form @submit.prevent="" class="upload-form">
+            <form @submit.prevent="processImage" class="upload-form">
                 <label for="itemName">Item Name:</label>
                 <input v-model="itemName" type="text" id="itemName" required>
 
                 <label for="image">Upload Image:</label>
-                <input type="file" ref="imageInput" accept="image/*" required>
+                <input type="file" ref="imageInput" accept="image/*" @change="handleImageChange" required>
+
+                <button type="button" @click="removeSelectedImage" v-if="selectedImage">Remove Selected Image</button>
 
                 <button type="submit">Submit</button>
+
             </form>
         </div>
     </div>
@@ -21,10 +24,26 @@ export default {
     data() {
         return {
             itemName: '',
+            selectedImage: null,
         };
     },
 
     methods: {
+        // Method to handle process image for face detection
+        async processImage() {
+        },
+        // Method to handle image change in the input
+        handleImageChange() {
+            // Update the selectedImage when the input file changes
+            const imageInput = this.$refs.imageInput;
+            this.selectedImage = imageInput.files[0];
+        },
+        // Method to remove the selected image
+        removeSelectedImage() {
+            this.selectedImage = null;
+            // Clear the file input to allow selecting the same file again
+            this.$refs.imageInput.value = '';
+        },
     },
 };
 </script>
