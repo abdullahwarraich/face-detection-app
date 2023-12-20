ba1b4c85e5fb4b4dc637a1f4fdcf8a1515b37e01
abdullahwarraich678 <abdullahwarraich678@gmail.com>
2023-12-20 20:32:28 +0000

    added authentication middleware to verify users access token

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { FaceDetectionModule } from './modules/faceDetection/faceDetection.module';
+import { AuthenticationMiddleware } from './middlewares/auth.middleware';
 @Module({
   imports: [AuthModule, FaceDetectionModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthenticationMiddleware).forRoutes('faceDetection');
+  }
+}
